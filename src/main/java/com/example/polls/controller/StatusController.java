8cdc274f0b014d2f67476385f6fedb353875c62f
blueskyxi3 <343854933@qq.com>
2021-04-21 21:38:32 +0800

    Update StatusController.java

--- src/main/java/com/example/polls/controller/StatusController.java
+++ src/main/java/com/example/polls/controller/StatusController.java
@@ -4,10 +4,6 @@ package com.example.polls.controller;
 import org.springframework.web.bind.annotation.*;
 
 
-/**
- * Created by rajeevkumarsingh on 20/11/17.
- */
-
 @RestController
 @RequestMapping("/api/_status/healthz")
 public class StatusController {
